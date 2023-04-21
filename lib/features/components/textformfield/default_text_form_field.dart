ce7e40a87e5bb44a78b825f9bfa37c3c6d0522c2
Kaan Uzman <55639112+akaanuzman@users.noreply.github.com>
2023-04-22 02:53:24 +0300

    i need help for issue #2 'export index'

--- lib/features/components/textformfield/default_text_form_field.dart
+++ lib/features/components/textformfield/default_text_form_field.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'index.dart';
 import '../../../core/theme/color/my_colors.dart';
-import 'special_text_form_field.dart';
 
 class DefaultTextFormField extends SpecialTextFormField {
   DefaultTextFormField({
---
+++ lib/features/components/textformfield/index.dart
@@ -0,0 +1,3 @@
+export 'default_text_form_field.dart';
+export 'otp_text_form_field.dart';
+export 'special_text_form_field.dart';
